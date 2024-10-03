92704b26e6be19003936ece7193cf6327bb142f2
danielgraciacamps <daniel.gracia.camps@estudiantat.upc.edu>
2024-10-03 11:29:21 +0200

    Update Problema1.R

--- Problema1.R
+++ Problema1.R
@@ -1,2 +1,11 @@
 #Codigo para problema 1
+#Codigo para problema 1
 
+# coef_deter=0.1923 lo vemos
+#coef_cor = sqrt(0.1923)
+# variacion de marketvalue en una unidad de cambio de assets = m =0.06576
+#que valor predecimos para assets=647.1
+m <- 0.06576
+b <- 11.79755
+
+m*(647.1) +11.79755
